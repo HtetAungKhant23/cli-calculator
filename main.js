c43e0c659f3e3b432d1575788f4d629548012ca1
Htet Aung Khant <htetaung23.ha@gmail.com>
2023-12-24 18:25:33 +0630

    4 method complete and using process.argv for passing arguments in the command line

--- main.js
+++ main.js
@@ -1,18 +1,42 @@
+const args = process.argv;
+const argv = args.slice(2);
+const firstNumber = +argv[0];
+const operator = argv[1];
+const secondNumber = +argv[2];
+
 class Calculator {
   constructor(firstNumber, secondNumber) {
     this.firstNumber = firstNumber;
     this.secondNumber = secondNumber;
   }
 
-  calculation(operator) {}
+  calculation() {}
 }
 
-class Adding extends Calculator {
+class Add extends Calculator {
   calculation() {
     return this.firstNumber + this.secondNumber;
   }
 }
 
-const add = new Adding(2, 4);
+class Subtract extends Calculator {
+  calculation() {
+    return this.firstNumber - this.secondNumber;
+  }
+}
+
+class Multiply extends Calculator {
+  calculation() {
+    return this.firstNumber * this.secondNumber;
+  }
+}
+
+class Divide extends Calculator {
+  calculation() {
+    return this.firstNumber / this.secondNumber;
+  }
+}
+
+const add = new Add(firstNumber, secondNumber);
 const res = add.calculation();
-console.log(res);
+console.log(`${firstNumber} ${operator} ${secondNumber} = ${res}`);
